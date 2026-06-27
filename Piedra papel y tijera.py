2ff0410df4fe631daaf800e162ba8fde814c1bac
Abel Proaño <abelchuros@gmail.com>
2026-06-27 18:11:20 -0500

    Update piedra papel y tijera.py add function game rule and add wins record

--- Piedra papel y tijera.py
+++ Piedra papel y tijera.py
@@ -1,27 +1,51 @@
 import random
+def game_rule(user:str, computer:str) -> str: 
+    rule = {
+        "piedra": "tijera",
+        "papel":  "piedra",
+        "tijera": "papel",
+    }
+    if user == computer:
+        return "empate"
+    if rule[user] == computer:
+        print("¡Ganaste!🥳🥳")
+        return "jugador"
+    print("😔  Perdiste  😔")
+    return "computadora"
 
-opciones = ["piedra", "papel", "tijera"]
+
+
+
+
+wins = 0
+options = ["piedra", "papel", "tijera"]
 print("¡¡BIENVENIDO AL JUEGO DE PIEDRA PAPEL Y TIJERA!!😎")
 while True:
-    jugador = input("Elige piedra, papel o tijera: ").lower()
-    computadora = random.choice(opciones)
-
-    print("La computadora eligió:", computadora)
-
-    if jugador != opciones:
-        print("ESCOJA UNA OPCIÓN VALIDA: piedra, papel o tijera:")
-    if jugador == computadora:
-        print("¡¡EMPATARON!!")
-    elif (jugador == "piedra" and computadora == "tijera") or \
-         (jugador == "papel" and computadora == "piedra") or \
-         (jugador == "tijera" and computadora == "papel"):
-        print("¡Ganaste!🥳🥳")
+    while True:
+        user = input("Elige piedra, papel o tijera: ").lower()  
+        if user not in ["piedra", "papel", "tijera"]:
+            print("Opcion invalida, elija nuevamente:")
+        else:
+            break
+    computer = random.choice(options)
+    print("La computadora eligió:", computer)
+    result = game_rule(user, computer)
+    if result == "empate":
+        print("¡Empate! Vuelve a elegir.")
+        continue  
+    if result == "jugador":
+        wins += 1
+        print(f"LLevas {wins} Victorias consecutivas")
     else:
-        print("😔  Perdiste  😔")
-
-    respuesta = input("¿Quieres volver a jugar? (si/no): ").lower()
-
-    if respuesta == "no":
-        print("¡Gracias por jugar GG!🤗")
+        if wins != 0:
+            print(f"¡Perdiste la racha de {wins} victorias") 
+        wins = 0
+    while True:
+        user = input("¿Quieres volver a jugar? (si/no): ").lower()
+        if user not in ["si", "no"]:
+            print("Opcion invalida, elija nuevamente:")
+        else:
+            break
+    if user == "no":
+        print("¡¡Gracias por jugar!!")
         break
-
